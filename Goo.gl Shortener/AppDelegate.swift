c3cdbe47a53ef2e515aebeec4e0744f2c8eaa96e
Aneesh <Aneesh@aneeshs-macbook-pro.local>
2016-02-10 00:22:26 +0530

    Initial Commit

---
+++ Goo.gl Shortener/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Goo.gl Shortener
+//
+//  Created by Aneesh on 10/02/16.
+//  Copyright © 2016 Aneesh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
